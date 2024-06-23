265bada23784bf0715d49689a2681cb49eaf2476
luck <2331898001@qq.com>
2024-06-23 11:41:42 +0800

    init

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-# http://127.0.0.1:5000/show/info
-@app.route("/show/info")
+# http://127.0.0.1:5000/index
+@app.route("/index")
 def index():
     # return "hello <h1>world</ h1>"
     return render_template("index.html")
